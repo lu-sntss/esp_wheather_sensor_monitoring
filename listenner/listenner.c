b5b0dc1fef3fc91bb32c3700ae5356d82a48a283
Lucas Araujo <47338414+lu-sntss@users.noreply.github.com>
2026-04-29 14:18:31 -0300

    Primeiro commit
    Feat:
    - Script para upload do ESP com configurações de WIfi e loop de monitoramento
    - Script em C para escutar os dados disparados pelo ESP e os enviar para aplicação Vercel

---
+++ listenner/listenner.c
@@ -0,0 +1,94 @@
+/*
+
+gcc listenner.c -o ouvinte -lmosquitto
+
+*/
+
+#include <stdio.h>
+#include <mosquitto.h>
+#include <curl/curl.h>
+
+const char* VERCEL_URL = "texte";
+
+// FUnção para empacotar o json e enviar ao vercel
+void enviar_para_vercel(const char* json_payload){
+    CURL *curl;
+    CURLcode response;
+
+    curl = curl_easy_init();
+    if (curl){
+        //COnfigura o cabeçalho, indicando q que o formato é JSON
+        struct curl_slist *headers = NULL;
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+
+        // Prepara o pacote
+        curl_easy_setopt(curl, CURLOPT_URL, VERCEL_URL);
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_payload);
+
+        response = curl_easy_perform(curl);
+
+        if (response != CURLE_OK){
+            fprintf(stderr, "[X] Erro no envio do pacote para o vercel: %s\n", curl_easy_strerror(response));
+        } else {
+            printf("Pacote enviado com sucesso\n");
+        }
+
+        // Limpa a memória
+        curl_slist_free_all(headers);
+        curl_easy_cleanup(curl);
+    }
+}
+
+// Função callback chamada quando uma mensagem chega no tópico/url assinado.
+void on_message(struct mosquitto *mosq, void *obj, const struct mosquitto_message *msg){
+    printf("Nova leitura recebida no tópico [%s]: %s\n", msg->topic, (char *)msg->payload);
+
+    enviar_para_vercel((char*)msg->payload);
+}
+
+// Função de callback chamada quando a exonexão com o broker deve ser estabelecida
+void on_connect(struct mosquitto *mosq, void *obj, int rc){
+    if (rc == 0){
+        printf("Dispositivo conectado ao Broker com sucesso!\n");
+        // Inscreve o url assim que conectar
+        mosquitto_subscribe(mosq, NULL, "estacao/dados",0);
+    } else {
+        fprintf(stderr,"Erro na conexxão: %s\n", mosquitto_connack_string(rc));
+    }
+}
+
+int main(){
+    struct mosquitto *mosq;
+    int rc;
+
+    // Inicializa a biblioteca cURL
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    // Inicializa biblioteca Mosquitto para comunicação
+    mosquitto_lib_init();
+
+    mosq = mosquitto_new("Workstation_Listener",true, NULL);
+    if (!mosq){
+        fprintf(stderr,"Erro ao criar instancia do Mosquitto.\n");
+        return 1;
+    }
+
+    // Define os callbacks
+    mosquitto_connect_callback_set(mosq,on_connect);
+    mosquitto_message_callback_set(mosq,on_message);
+
+    rc = mosquitto_connect(mosq, "localhost", 1883, 60);
+    if(rc != MOSQ_ERR_SUCCESS){
+        mosquitto_destroy(mosq);
+        fprintf(stderr,"Erro ao conectar: %s\n", mosquitto_strerror(rc));
+        return 1;
+    }
+
+    mosquitto_loop_forever(mosq, -1,1);
+
+    mosquitto_destroy(mosq);
+    mosquitto_lib_cleanup();
+    curl_global_cleanup();
+    return 0;
+}
---
+++ wheather_esp/src/main.cpp
@@ -0,0 +1,133 @@
+/*
+// Adicionando o escutar externo do mosquito no Ubuntu
+sudo nano /etc/mosquitto/conf.d/teste.conf
+
+listener 1883 0.0.0.0
+allow_anonymous true
+
+
+// Liberar porta temporariamente
+sudo chmod a+rw /dev/ttyUSB0
+
+// Adicionando passe no firewall
+sudo ufw allow 1883/tcp
+
+sudo systemctl restart mosquitto
+
+// Deletar passe no firewall
+sudo ufw delete allow 1883/tcp
+*/
+
+#include "HardwareSerial.h"
+#include <Arduino.h> // Obrigatório no C++ padrão do PlatformIO
+#include <Wire.h>
+#include <BH1750.h>
+#include <ESP8266WiFi.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+#include <PubSubClient.h>
+
+// Constantes de conexão
+const char* ssid = "N305-3033124-LL-lu"; 
+const char* password = "sonic301101"; 
+const char* mqtt_server = "10.42.0.1"; // O IP do notebook
+
+// Instancia os objetos
+WiFiClient espClient;
+PubSubClient client(espClient);
+BH1750 lightMeter;
+Adafruit_BMP280 bmp;
+
+// Variáveis de tempo para não travar o loop
+unsigned long ultima_leitura = 0;
+const long intervalo = 5000;
+const long intervalo_leitura = 2000;
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Conectando ao WiFi: ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("WiFi conectado! IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+  Serial.println(F("\n--- Iniciando Estacao Meteorologica via PlatformIO ---"));
+
+  setup_wifi();
+  
+  // Configura o MQTT
+  client.setServer(mqtt_server, 1883);
+
+  Wire.begin();
+  
+  // Verifica e Inicia Sensores
+  if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
+    Serial.println(F("[OK] Sensor BH1750 (Luz) pronto!"));
+  } else {
+    Serial.println(F("[ERRO] Falha no BH1750."));
+  }
+
+  if (bmp.begin(0x76)) {
+    Serial.println(F("[OK] Sensor BMP280 (Clima) pronto!"));
+  } else {
+    Serial.println(F("[ERRO] Falha no BMP280."));
+  }
+}
+
+void loop() {
+  // Mantém ou reconecta no MQTT do Ubuntu
+  if (!client.connected()) {
+    unsigned long contagem_tentativa = 1;
+    while (!client.connected()) {
+      Serial.print("Tentando conectar ao Mosquitto MQTT...");
+      Serial.print(" (Tentativa ");
+      Serial.print(contagem_tentativa);
+      Serial.print(")");
+      if (client.connect("ESP8266_Estacao")) {
+        Serial.println("\nConectado!");
+      } else {
+        Serial.print("\nFalha, rc=");
+        Serial.print(client.state());
+        Serial.println("Tentando de novo em 5 segundos...");
+        delay(5000);
+        contagem_tentativa++;
+        
+      }
+    }
+  }
+  
+  client.loop(); // Processa a conexão MQTT em background
+
+  // Envia as leituras a cada 5 segundos sem travar o processador (sem delay)
+  unsigned long tempo_atual = millis();
+  if (tempo_atual - ultima_leitura >= intervalo_leitura) {
+    ultima_leitura = tempo_atual;
+
+    float lux = lightMeter.readLightLevel();
+    float temp = bmp.readTemperature();
+    float press = bmp.readPressure() / 100.0F;
+
+    // Monta o pacote no braço
+    char payload[100];
+    snprintf(payload, sizeof(payload), "{\"temp\": %.2f, \"press\": %.2f, \"luz\": %.2f}", temp, press, lux);
+
+    // Serial.print("Enviando dados pro Ubuntu: ");
+    Serial.println(payload);
+    
+    // Grita os dados no tópico
+    client.publish("estacao/dados", payload);
+  }
+}
